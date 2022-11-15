bf60b0d1bc2232ff1b9363de8a5e9ca5f74b5306
leh <leandro.programmer@hotmail.com>
2022-11-15 15:42:15 -0300

    feat: Adding crm page structure

--- src/pages/crm.tsx
+++ src/pages/crm.tsx
@@ -1,5 +1,27 @@
+const questions = [
+  { question: 'Possui Lição da Escola Sabatina?' },
+  { question: 'Estudou a Bíblia, ou a Lição durante a semana?' },
+  { question: 'Ajudou alguém durante essa semana?' }
+]
+
 export default function Crm () {
   return (
-    <h1 className='text-white'>Crm</h1>
+    <div className='w-1/2 flex flex-col self-center gap-20'>
+      {questions.map((question, index) => (
+        <div key={index} className='flex flex-col gap-3'>
+          <div className='text-white text-xl bg-primary bg-opacity-10 p-10 rounded-xl'>
+            <span>{question.question}</span>
+          </div>
+
+          <div className='flex gap-3'>
+            <button className='h-10 w-full bg-bg-hover rounded-xl text-primary'>Não</button>
+            <button className='h-10 w-full bg-primary rounded-xl text-white'>Sim</button>
+          </div>
+        </div>
+      ))}
+
+      <button className='h-10 w-full bg-primary rounded-xl text-white'>Enviar</button>
+    </div>
+
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
         'custom-gradient': 'linear-gradient(150deg, rgba(14,14,16,1) 45%, rgba(20,50,55,1) 70%, rgba(14,14,16,1) 100%)'
       },
       backgroundColor: {
-        'bg-hover': '#161616'
+        'bg-hover': '#161616',
+        primary: '#027C91'
       },
       textColor: {
         white: '#FFFFFF',
